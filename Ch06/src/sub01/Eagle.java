15d181a8ddc048e954e442b5bb5f0f206370f7d1
heo <m8765000@naver.com>
2026-05-11 09:48:54 +0900

    예외처리

---
+++ Ch06/src/sub01/Eagle.java
@@ -0,0 +1,10 @@
+package sub01;
+
+public class Eagle extends Animal {
+	public void move() {
+		System.out.println("Eagle Fly...");
+	}
+	public void hunt() {
+		System.out.println("Eagle hunt");
+	}
+}
